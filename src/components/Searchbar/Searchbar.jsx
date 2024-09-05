76a26c74efa061252615202080d80f52a3acc2b9
AryanBhardwaj789 <aryanbhardwaj98126@gmail.com>
2024-09-04 23:54:08 -0400

    Implemented Responsive Design in Searchbar Component

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react';
 import './Searchbar.css';
 
@@ -32,39 +31,38 @@ const Searchbar = () => {
     console.log(`Searching Yelp with ${searchInput}, ${locationInput}, ${sortingOption}`);
   };
   
-  
   return (
-    <div className="w-full mx-auto p-28 bg-white bg-[url('/public/background_search_desktop.jpg')] bg-cover bg-center">
-      <div className="flex flex-row justify-evenly mb-16 border-b-2 border-gray-300">
+    <div className="w-full mx-auto p-6 sm:p-10 lg:p-28 bg-white bg-[url('/public/background_search_desktop.jpg')] bg-cover bg-center">
+      <div className="flex flex-col lg:flex-row lg:justify-evenly mb-6 lg:mb-16 border-b-2 border-gray-300">
         <span
-          className={`text-white text-lg font-poppins font-medium tracking-tight py-4 cursor-pointer sort-option ${getSortOptionClass('best_match')}`}
+          className={`text-white text-lg font-poppins font-medium tracking-tight py-2 lg:py-4 cursor-pointer sort-option ${getSortOptionClass('best_match')}`}
           onClick={() => handleSortOptionClick('best_match')}
         >
           Best Match
         </span>
         <span
-          className={`text-white text-lg font-poppins font-medium tracking-tight py-4 cursor-pointer sort-option ${getSortOptionClass('highest_rated')}`}
+          className={`text-white text-lg font-poppins font-medium tracking-tight py-2 lg:py-4 cursor-pointer sort-option ${getSortOptionClass('highest_rated')}`}
           onClick={() => handleSortOptionClick('highest_rated')}
         >
           Highest Rated
         </span>
         <span
-          className={`text-white text-lg font-poppins font-medium tracking-tight py-4 cursor-pointer sort-option ${getSortOptionClass('most_reviewed')}`}
+          className={`text-white text-lg font-poppins font-medium tracking-tight py-2 lg:py-4 cursor-pointer sort-option ${getSortOptionClass('most_reviewed')}`}
           onClick={() => handleSortOptionClick('most_reviewed')}
         >
           Most Reviewed
         </span>
       </div>
-      <div className="flex flex-row space-x-6 gap-16 px-5">
+      <div className="flex flex-col lg:flex-row lg:space-x-6 gap-6 lg:gap-16 px-5">
         <input
-          className="w-2/3 p-2 border border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-700"
+          className="w-full lg:w-2/3 p-2 border border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-700"
           type="text"
           placeholder="Search Businesses"
           value={searchInput}
           onChange={handleSearchInputChange}
         />
         <input
-          className="w-2/3 p-2 border border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-700"
+          className="w-full lg:w-2/3 p-2 border border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-700"
           type="text"
           placeholder="Where?"
           value={locationInput}
@@ -72,7 +70,7 @@ const Searchbar = () => {
         />
       </div>
       <div className='mt-6 text-center font-poppins font-semibold tracking-wide'>
-        <button onClick={handleSearchButtonClick} className="w-fit py-2 px-8 bg-yellow-600 text-white rounded-md hover:bg-yellow-800 focus:outline-none focus:ring-2 focus:ring-yellow-700">
+        <button onClick={handleSearchButtonClick} className="w-full lg:w-fit py-2 px-8 bg-yellow-600 text-white rounded-md hover:bg-yellow-800 focus:outline-none focus:ring-2 focus:ring-yellow-700">
           Let's Go
         </button>
       </div>
